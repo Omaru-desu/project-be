4c87f088a0448042e6fe55a491c21c0da13936bc
rfadillaa <r.fadilla@student.uq.edu.au>
2026-04-10 20:50:30 +1000

    fear: add delete projects feature

--- app/projects.py
+++ app/projects.py
@@ -127,3 +127,22 @@ def update_project(
         raise HTTPException(status_code=404, detail="Project not found")
 
     return result.data[0]
+
+@router.delete("/projects/{project_id}")
+def delete_project(
+    project_id: str,
+    user_id: str = Depends(get_current_user)
+):
+    result = (
+        supabase
+        .table("projects")
+        .delete()
+        .eq("id", project_id)
+        .eq("owner", user_id)
+        .execute()
+    )
+
+    if not result.data:
+        raise HTTPException(status_code=404, detail="Project not found")
+
+    return {"message": "Deleted"}
